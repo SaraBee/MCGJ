06f94c39a6e8e7a9f47b95cbd22abc7eb0db365c
Sara Bee <sarabobo@gmail.com>
2020-08-19 04:37:37 +0000

    add spotify util class

---
+++ mcgj/spotify.py
@@ -0,0 +1,17 @@
+import spotipy
+import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials
+
+class Spotify:
+    def __init__(self):
+        auth_manager = SpotifyClientCredentials()
+        self.sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    def getTrackInfo(self, url):
+        track = self.sp.track(url)
+        print(track)
+        title = track['name']
+        artist = track['artists'][0]['name']
+        return title, artist
+
+
