706ab7222d9163748e13a24ac5f1c5cb037ffbd2
Matt Long <matthewhl@gmail.com>
2022-05-15 21:03:21 -0400

    add js

---
+++ mcgj/static/js/main.js
@@ -0,0 +1,55 @@
+class Song extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  onmouseleave = () => {
+    const el = document.querySelector(
+      `album-art.art-link[data-trackid="${id}"]`
+    );
+    el.toggleAttribute("highlighted", true);
+  };
+
+  onmouseenter = () => {
+    const el = document.querySelector(
+      `album-art.art-link[data-trackid="${id}"]`
+    );
+    el.toggleAttribute("highlighted", false);
+  };
+
+  disconnectedCallback() {
+    console.log("disconnected");
+  }
+}
+
+class RoundContainer extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    // if (this.isConnected) {
+    //   const song = this.querySelector("session-song");
+    //   console.log(song.textContent);
+    // }
+  }
+}
+
+class AlbumArt extends HTMLAnchorElement {
+  static get observedAttributes() {
+    return ["highlighted"];
+  }
+
+  constructor() {
+    super();
+  }
+
+  attributeChangedCallback(name, oldVal, newVal) {
+    console.log({ name, oldVal, newVal });
+  }
+}
+customElements.define("session-song", Song);
+customElements.define("round-container", RoundContainer);
+customElements.define("album-art", AlbumArt, { extends: "a" });
